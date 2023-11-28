746c2072bcd1dfb227a8e5aed77ea27c2c7d7745
kangharyeom <kangharyeom@gmail.com>
2023-11-29 08:42:52 +0900

    Refactor: API 주소 소문자로 변경

--- src/main/java/com/my_bank/myBank/domain/account/controller/AccountController.java
+++ src/main/java/com/my_bank/myBank/domain/account/controller/AccountController.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.Positive;
 @Validated
 @RequiredArgsConstructor
 @Log4j2
-@RequestMapping("/api/Accounts")
+@RequestMapping("/api/accounts")
 public class AccountController {
 
     private final AccountService accountService;
