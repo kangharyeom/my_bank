dfc96f9f0358d002bed73c51b32ed6f216d6e4fa
kangharyeom <kangharyeom@gmail.com>
2023-11-28 17:00:15 +0900

    Feat: OutComeCategory
    - 지출 내역 카테고리 추가

---
+++ src/main/java/com/my_bank/myBank/global/constant/OutComeCategory.java
@@ -0,0 +1,8 @@
+package com.my_bank.myBank.global.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum OutComeCategory {
+    SHOPPING, BANK_TRANSFER, PHONE, TRANSPORTATION_EXPENSES, EATING_OUT, LEISURE, ETC
+}
